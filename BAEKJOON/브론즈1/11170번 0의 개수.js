5b1184d9955f5450c55c1285c30a5d45ce4498b8
hafnium1923 <hafnium1923@gmail.com>
2023-07-11 09:49:08 +0900

    11170번 0의 개수

---
+++ BAEKJOON/브론즈1/11170번 0의 개수.js
@@ -0,0 +1,24 @@
+let [num, ...rest] = require("fs")
+  .readFileSync("example.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+let testCase = [];
+
+rest.forEach((i) => {
+  testCase.push(i.trim().split(" ").map(Number));
+});
+
+testCase.forEach((test, index) => {
+  let count = 0;
+  for (let i = test[0]; i <= test[1]; i++) {
+    if (i === 0) count++;
+    let j = i;
+    while (j > 0) {
+      if (j % 10 === 0) count++;
+      j = Math.floor(j / 10);
+    }
+  }
+  console.log(count);
+});
