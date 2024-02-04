51950f39e84954a8a4eb543a99b6afb99f239bba
Rulu <hafnium1923@gmail.com>
2024-02-04 14:03:50 +0900

    Create 1260번 DFS와 BFS.js

---
+++ BAEKJOON/실버2/1260번 DFS와 BFS.js
@@ -0,0 +1,65 @@
+const { start } = require("repl");
+
+let [info, ...vertexInfo] = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "example.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [vertextNum, nodeNum, startVertex] = info.split(" ").map(Number);
+const nodeList = Array.from({ length: vertextNum + 1 }, () => []);
+
+vertexInfo.forEach((info) => {
+  const [V1, V2] = info.split(" ").map(Number);
+  nodeList[V1].push(V2);
+  nodeList[V2].push(V1);
+});
+
+nodeList.forEach((node) => {
+  node.sort((a, b) => a - b);
+});
+
+const dfsVisited = Array.from({ length: vertextNum + 1 }, () => false);
+const dfsAnswer = [];
+const bfsVisited = Array.from({ length: vertextNum + 1 }, () => false);
+const bfsAnswer = [];
+
+dfs(startVertex);
+bfs(startVertex);
+console.log(dfsAnswer.join(" ") + "\n" + bfsAnswer.join(" "));
+
+function dfs(vertexNo) {
+  dfsVisited[vertexNo] = true;
+  dfsAnswer.push(vertexNo);
+
+  for (let i = 0; i < nodeList[vertexNo].length; i++) {
+    const next = nodeList[vertexNo][i];
+
+    if (!dfsVisited[next]) {
+      dfs(next);
+    }
+  }
+}
+
+function bfs(vertexNo) {
+  const queue = [];
+  let queueIndex = 0;
+  queue.push(vertexNo);
+  bfsVisited[vertexNo] = true;
+
+  while (queueIndex !== queue.length) {
+    const current = queue[queueIndex];
+    bfsAnswer.push(current);
+
+    for (let i = 0; i < nodeList[current].length; i++) {
+      const next = nodeList[current][i];
+
+      if (!bfsVisited[next]) {
+        queue.push(next);
+        bfsVisited[next] = true;
+      }
+    }
+
+    queueIndex++;
+  }
+}
